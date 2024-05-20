e31b5eed92be0c52bc428671cac076ddff0e2551
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-20 18:38:15 +0200

    added main

---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,15 @@
+<div id="hero"></div>
+<main>
+  <div class="container">
+    <h3>Current series</h3>
+    <div class="row">
+      @foreach ($comics as $card)
+    @include('partials.card')
+  @endforeach
+    </div>
+  </div>
+  <div class="load d-flex justify-content-center align-items-center">
+    <button>Load More</button>
+  </div>
+  </div>
+</main>
