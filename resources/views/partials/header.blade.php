dc827b9bbcca2f55fe955f936eac6a50ffd11134
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-20 17:15:01 +0200

    added header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header class="container">
+    <nav class="d-flex justify-content-center align-items-center py-3">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC logo" />
+        <ul class="d-flex">
+            <li>
+                @foreach ($links as $link)
+                        <a class="{{Route::currentRouteName() == 'home' ? 'active' : ''}}"
+                            href="{{route('home')}}">{{$link}}</a>
+                    </li>
+                @endforeach
+        </ul>
+    </nav>
+</header>
