b648ced22a117e933b44e6b87c10c737ac4ec286
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-20 18:37:57 +0200

    added 2 array to Route function

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $links = config('nav');
+    $comics = config('comics');
+    return view('home', compact('links'), compact('comics'));
+})->name('home');
