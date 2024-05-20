bef7585b3224791a7e259e55dbe0b5d31877dd47
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-20 17:07:09 +0200

    added nav array

---
+++ config/nav.php
@@ -0,0 +1,13 @@
+<?php
+return [
+        "characters" => "Characters",
+        "comics" => "Comics",
+        "movies" => "Movies",
+        "tv" => "TV",
+        "games" => "Games",
+        "collectibles" => "Collectibles",
+        "videos" => "Videos",
+        "fans" => "Fans",
+        "news" => "News",
+        "shop" => "Shop",
+];
