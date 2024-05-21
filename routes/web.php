867db5f44f78e3352b22539f1174aa21ed7bd8b3
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-21 10:36:30 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $links = config('nav');
     $comics = config('comics');
-    return view('home', compact('links'), compact('comics'));
+    return view('home', compact('links', 'comics'));
 })->name('home');
